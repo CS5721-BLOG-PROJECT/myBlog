5194f8a9f1aa222e3a39c829f9c3f67f29dfb2c5
jtc23 <936865249@qq.com>
2020-11-30 21:15:03 +0800

    write the logic of hrRole and hr interface and api of hr

--- myBlog-service/src/main/java/com/myBlog/service/HrService.java
+++ myBlog-service/src/main/java/com/myBlog/service/HrService.java
@@ -1,7 +1,9 @@
 package com.myBlog.service;
 
 import com.myBlog.mapper.HrMapper;
+import com.myBlog.mapper.HrRoleMapper;
 import com.myBlog.model.Hr;
+import com.myBlog.service.utils.HrUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,8 +18,8 @@ import java.util.List;
 public class HrService implements UserDetailsService {
     @Autowired
     HrMapper hrMapper;
-//    @Autowired
-//    HrRoleMapper hrRoleMapper;
+    @Autowired
+    HrRoleMapper hrRoleMapper;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -29,47 +31,47 @@ public class HrService implements UserDetailsService {
         return hr;
     }
 
-//    public List<Hr> getAllHrs(String keywords) {
-//        return hrMapper.getAllHrs(HrUtils.getCurrentHr().getId(),keywords);
-//    }
-//
-//    public Integer updateHr(Hr hr) {
-//        return hrMapper.updateByPrimaryKeySelective(hr);
-//    }
-//
-//    @Transactional
-//    public boolean updateHrRole(Integer hrid, Integer[] rids) {
-//        hrRoleMapper.deleteByHrid(hrid);
-//        return hrRoleMapper.addRole(hrid, rids) == rids.length;
-//    }
-//
-//    public Integer deleteHrById(Integer id) {
-//        return hrMapper.deleteByPrimaryKey(id);
-//    }
-//
-//    public List<Hr> getAllHrsExceptCurrentHr() {
-//        return hrMapper.getAllHrsExceptCurrentHr(HrUtils.getCurrentHr().getId());
-//    }
-//
-//    public Integer updateHyById(Hr hr) {
-//        return hrMapper.updateByPrimaryKeySelective(hr);
-//    }
-//
-//    public boolean updateHrPasswd(String oldpass, String pass, Integer hrid) {
-//        Hr hr = hrMapper.selectByPrimaryKey(hrid);
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        if (encoder.matches(oldpass, hr.getPassword())) {
-//            String encodePass = encoder.encode(pass);
-//            Integer result = hrMapper.updatePasswd(hrid, encodePass);
-//            if (result == 1) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    public Integer updateUserface(String url, Integer id) {
-//        return hrMapper.updateUserface(url, id);
-//    }
+    public List<Hr> getAllHrs(String keywords) {
+        return hrMapper.getAllHrs(HrUtils.getCurrentHr().getId(),keywords);
+    }
+
+    public Integer updateHr(Hr hr) {
+        return hrMapper.updateByPrimaryKeySelective(hr);
+    }
+
+    @Transactional
+    public boolean updateHrRole(Integer hrid, Integer[] rids) {
+        hrRoleMapper.deleteByHrid(hrid);
+        return hrRoleMapper.addRole(hrid, rids) == rids.length;
+    }
+
+    public Integer deleteHrById(Integer id) {
+        return hrMapper.deleteByPrimaryKey(id);
+    }
+
+    public List<Hr> getAllHrsExceptCurrentHr() {
+        return hrMapper.getAllHrsExceptCurrentHr(HrUtils.getCurrentHr().getId());
+    }
+
+    public Integer updateHyById(Hr hr) {
+        return hrMapper.updateByPrimaryKeySelective(hr);
+    }
+
+    public boolean updateHrPasswd(String oldpass, String pass, Integer hrid) {
+        Hr hr = hrMapper.selectByPrimaryKey(hrid);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if (encoder.matches(oldpass, hr.getPassword())) {
+            String encodePass = encoder.encode(pass);
+            Integer result = hrMapper.updatePasswd(hrid, encodePass);
+            if (result == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer updateUserface(String url, Integer id) {
+        return hrMapper.updateUserface(url, id);
+    }
 }
 

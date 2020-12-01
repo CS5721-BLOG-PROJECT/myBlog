070b7e4d44a77b5840d1a3af4fcb56d301eb766f
郝雷 <m13522389185@163.com>
2020-12-02 07:03:45 +0800

    cancel Cacheable

--- myBlog-service/src/main/java/com/myBlog/service/MenuService.java
+++ myBlog-service/src/main/java/com/myBlog/service/MenuService.java
@@ -24,19 +24,19 @@ public class MenuService {
         return menuMapper.getMenusByHrId(((Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
     }
 
-    @Cacheable
+//    @Cacheable
     public List<Menu> getAllMenusWithRole() {
         return menuMapper.getAllMenusWithRole();
     }
-
+//
     public List<Menu> getAllMenus() {
         return menuMapper.getAllMenus();
     }
-
+//
     public List<Integer> getMidsByRid(Integer rid) {
         return menuMapper.getMidsByRid(rid);
     }
-
+//
     @Transactional
     public boolean updateMenuRole(Integer rid, Integer[] mids) {
         menuRoleMapper.deleteByRid(rid);
